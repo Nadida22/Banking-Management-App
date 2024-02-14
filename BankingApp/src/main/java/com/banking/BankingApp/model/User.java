f4c6b88cda6c6eaefac75bb600cebc0204db63c7
jojobee333 <symone.j.bates@gmail.com>
2024-02-14 16:42:44 -0500

    Added  fields in User and Transaction Model classes.  Added Enums

--- BankingApp/src/main/java/com/banking/BankingApp/model/User.java
+++ BankingApp/src/main/java/com/banking/BankingApp/model/User.java
@@ -1,5 +1,13 @@
 package com.banking.BankingApp.model;
 
+import com.banking.BankingApp.model.enums.UserRole;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
 public class User {
 
     // TODO: add fields
@@ -14,5 +22,23 @@ public class User {
     // firstname -- String
     // lastname -- String
     // accounts -- HashSet -- OnetoMany relationship to Account
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Account> accounts = new HashSet<>();
+
+
+
 
 }
---
+++ BankingApp/src/main/java/com/banking/BankingApp/model/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.banking.BankingApp.model.enums;
+
+public enum UserRole {
+    USER, ADMIN
+}
